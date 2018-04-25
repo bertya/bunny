18c4407ed8da524883165e5f02bf15625e9b2adf
bertya <bert_b@live.com>
2018-04-25 09:56:19 -0400

    Bug fix

--- src/containers/post_new.js
+++ src/containers/post_new.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { ReactMic } from 'react-mic';
-import AceEditor from 'react-ace';
-import 'brace/mode/java';
-import 'brace/mode/javascript'
-import 'brace/mode/c_cpp'
-import 'brace/theme/tomorrow';
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { ReactMic } from "react-mic";
+import AceEditor from "react-ace";
+import "brace/mode/java";
+import "brace/mode/javascript";
+import "brace/mode/c_cpp";
+import "brace/theme/tomorrow";
 
-import { createPost } from '../actions/action_posts';
+import { createPost } from "../actions/action_posts";
 
 class PostNew extends Component {
   constructor(props) {
@@ -18,7 +18,7 @@ class PostNew extends Component {
       record: false,
       blob: null,
       timer: 90,
-      mode: 'java',
+      mode: "java"
     };
   }
 
@@ -29,27 +29,23 @@ class PostNew extends Component {
   }
 
   renderInput(field) {
-    const className = 'form-group';
-    const { meta: { touched, error } } = field;
-    let klass = `form-control ${touched && error ? 'is-invalid' : ''}`;
+    const className = "form-group";
+    const {
+      meta: { touched, error }
+    } = field;
+    let klass = `form-control ${touched && error ? "is-invalid" : ""}`;
     return (
       <div className={className}>
         <label>{field.label}</label>
-        <input
-          className={klass}
-          type="text"
-          {...field.input}
-        />
-        <div className="invalid-feedback">
-          {touched ? error : ''}
-        </div>
+        <input className={klass} type="text" {...field.input} />
+        <div className="invalid-feedback">{touched ? error : ""}</div>
       </div>
     );
   }
 
   renderSelect(field) {
-    const className = 'form-group';
-    console.log('???')
+    const className = "form-group";
+    console.log("???");
     // this.setState({
     //   mode: field.input.value
     // });
@@ -66,23 +62,21 @@ class PostNew extends Component {
     );
   }
 
-
   renderNote(field) {
     return (
       <div className="form-group">
         <label htmlFor="content">Leave additonal note</label>
-        <textarea
-          className="form-control form-textarea"
-          {...field.input}
-        ></textarea>
+        <textarea className="form-control form-textarea" {...field.input} />
       </div>
     );
   }
 
   renderEditor(field, props) {
-    const { meta: { touched, error } } = field;
-    const className = `form-group ${touched && error ? 'is-invalid' : ''}`;
-    const aceOnBlur = (onBlur) => (_event, editor) => {
+    const {
+      meta: { touched, error }
+    } = field;
+    const className = `form-group ${touched && error ? "is-invalid" : ""}`;
+    const aceOnBlur = onBlur => (_event, editor) => {
       if (!editor) return;
       const value = editor.getValue();
       onBlur(value);
@@ -102,18 +96,21 @@ class PostNew extends Component {
           value={field.input.value}
           editorProps={{ $blockScrolling: true }}
         />
-        <div className="invalid-feedback">
-          {touched ? error : ''}
-        </div>
+        <div className="invalid-feedback">{touched ? error : ""}</div>
       </div>
     );
   }
 
   onSubmit(values) {
     values.mode = this.state.mode;
-    this.props.createPost(values, this.state.blob, this.props.authedUser, (postId) => {
-      this.props.history.push(`/posts/${postId}`);
-    });
+    this.props.createPost(
+      values,
+      this.state.blob,
+      this.props.authedUser,
+      postId => {
+        this.props.history.push(`/posts/${postId}`);
+      }
+    );
   }
 
   startRecording = () => {
@@ -121,16 +118,17 @@ class PostNew extends Component {
       record: true,
       timer: 90
     });
-
+    clearInterval(this.countDown);
     this.countDown = setInterval(() => {
       if (this.state.timer === 0) {
+        this.stopRecording();
         clearInterval(this.countDown);
       }
       this.setState({
         timer: this.state.timer - 1
-      })
+      });
     }, 1000);
-  }
+  };
 
   stopRecording = () => {
     this.setState({
@@ -138,25 +136,24 @@ class PostNew extends Component {
     });
 
     clearInterval(this.countDown);
-  }
+  };
 
-  onStop = (recordedBlob) => {
+  onStop = recordedBlob => {
     this.setState({
       blob: recordedBlob
     });
-  }
+  };
 
   render() {
     const { handleSubmit } = this.props;
 
     return (
       <div>
-        <form onSubmit={handleSubmit(this.onSubmit.bind(this))} className="mt-3">
-          <Field
-            label="Title"
-            name="title"
-            component={this.renderInput}
-          />
+        <form
+          onSubmit={handleSubmit(this.onSubmit.bind(this))}
+          className="mt-3"
+        >
+          <Field label="Title" name="title" component={this.renderInput} />
           <Field
             label="Mode"
             name="mode"
@@ -169,16 +166,12 @@ class PostNew extends Component {
             component={this.renderEditor}
             currentMode={this.state.mode}
           />
-          <Field
-            label="Note"
-            name="note"
-            component={this.renderNote}
-          />
+          <Field label="Note" name="note" component={this.renderNote} />
           <div className="m-audio form-group">
             <label>Add audio to explain your thoughts</label>
             <div className="m-audio__wrap">
               <div className="m-audio__timer">
-                <div >{this.state.timer}s</div>
+                <div>{this.state.timer}s</div>
               </div>
               <div className="m-audio__wave">
                 <ReactMic
@@ -186,20 +179,39 @@ class PostNew extends Component {
                   className="sound-wave"
                   onStop={this.onStop}
                   strokeColor="#484848"
-                  backgroundColor="#FFFFFF" />
+                  backgroundColor="#FFFFFF"
+                />
               </div>
             </div>
             <div className="btn-group mt-1">
-              <button onClick={this.startRecording} type="button" className="btn btn-success btn-sm">Start</button>
-              <button onClick={this.stopRecording} type="button" className="btn btn-danger btn-sm">Stop</button>
+              <button
+                onClick={this.startRecording}
+                type="button"
+                className="btn btn-success btn-sm"
+              >
+                Start
+              </button>
+              <button
+                onClick={this.stopRecording}
+                type="button"
+                className="btn btn-danger btn-sm"
+              >
+                Stop
+              </button>
             </div>
-            <audio className="mt-1" controls src={this.state.blob ? this.state.blob.blobURL : ''}></audio>
+            <audio
+              className="mt-1"
+              controls
+              src={this.state.blob ? this.state.blob.blobURL : ""}
+            />
           </div>
           <div className="mt-5">
-            <button type="submit" className="btn btn-dark">Submit</button>
-            <Link to={'/'} className="btn btn-outline-secondary ml-2">
+            <button type="submit" className="btn btn-dark">
+              Submit
+            </button>
+            <Link to={"/"} className="btn btn-outline-secondary ml-2">
               Home
-          </Link>
+            </Link>
           </div>
         </form>
       </div>
@@ -211,11 +223,11 @@ function validate(values) {
   const errors = {};
 
   if (!values.title) {
-    errors.title = 'Cannot be empty';
+    errors.title = "Cannot be empty";
   }
 
   if (!values.code) {
-    errors.code = 'Cannot be empty';
+    errors.code = "Cannot be empty";
   }
 
   return errors;
@@ -229,8 +241,5 @@ function mapStateToProps(state) {
 
 export default reduxForm({
   validate,
-  form: 'PostNewForm'
-})(
-  connect(mapStateToProps, { createPost })(PostNew)
-);
-
+  form: "PostNewForm"
+})(connect(mapStateToProps, { createPost })(PostNew));
